286566e7103eea68720808dc37e920c4e07d8d1a
Tyler Thompson <tyler@digitalsummit.net>
2017-12-12 16:21:16 -0700

    Temporary push to force reset of capitalization on file names.

---
+++ zMUGStore.Ui.XamarinForms/zMUGStore.Ui.XamarinForms/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace NMUGStore.Ui.XamarinForms
+{
+    public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
